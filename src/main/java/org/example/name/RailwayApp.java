c29333e93189f5e4f1ed74c6a8285d88abb0f724
Saray Mendivelso <zaryz2005@gmail.com>
2025-02-22 19:46:44 -0500

    Trabajo finalizado

---
+++ src/main/java/org/example/name/RailwayApp.java
@@ -0,0 +1,9 @@
+package org.example.name;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class RailwayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayApp.class, args);
+    }
+}
