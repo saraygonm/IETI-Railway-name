ddcdd8b3ff994c4a9f222a49afdcd6028ef36c02
Saray Alieth Mendivelso Gonzalez <111905625+saraygonm@users.noreply.github.com>
2025-02-22 22:19:48 -0500

    Update RailwayApp.java

--- src/main/java/org/example/name/RailwayApp.java
+++ src/main/java/org/example/name/RailwayApp.java
@@ -1,15 +1,9 @@
 package org.example.name;
-<<<<<<< HEAD
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-=======
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
->>>>>>> c29333e93189f5e4f1ed74c6a8285d88abb0f724
 public class RailwayApp {
     public static void main(String[] args) {
         SpringApplication.run(RailwayApp.class, args);
